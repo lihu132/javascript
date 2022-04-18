d689dd2e3515a7afdcb56e6ac56f0c8c260d0356
lihu132 <lihu132@gmail.com>
2022-04-18 23:38:45 +1000

    316_animate

---
+++ 316_animate.js
@@ -0,0 +1,24 @@
+function animate(obj, target, callback) {
+    console.log(callback); //callback = function(){} 调用的时候 callback()
+
+    clearInterval(obj.timer)
+    obj.timer = setInterval(function () {
+        var step = (target - obj.offsetLeft) / 10
+        step = step > 0 ? Math.ceil(step) : Math.floor(step)
+        if (obj.offsetLeft == target) {
+            clearInterval(obj.timer)
+            //回调函数写在定时器结束里面
+            if (callback) {
+                callback()
+            }
+        }
+        else {
+            //缓动，更改步长
+            //匀速动画 步长固定
+            //缓动动画 变化步长
+            //把步长值改为整数
+            obj.style.left = obj.offsetLeft + step + 'px'
+        }
+
+    }, 15)
+}
