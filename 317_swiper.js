6e17ed5fac876c6d14cfa5b47e85530dbcf33a91
leo-outside <morinbrit@gmail.com>
2022-04-20 12:03:42 +1000

    dynamic_circles

--- 317_swiper.js
+++ 317_swiper.js
@@ -3,7 +3,10 @@ window.addEventListener('load', function() {
     var arrow_l = this.document.querySelector('.arrow-l')
     var arrow_r = this.document.querySelector('.arrow-r')
     var focus = this.document.querySelector('.focus')
-
+    var ul = this.document.querySelector('ul')
+    var circle = this.document.querySelector('.circle')
+    var img = this.document.querySelector('img')
+    ul.style.width = img.offsetWidth * ul.children.length + 'px'
 
     //1.鼠标经过focus 显示左右箭头
     focus.addEventListener('mouseenter', function() {
@@ -17,5 +20,8 @@ window.addEventListener('load', function() {
 
 
     //2.动态生成小圆圈
-
+    for (var i = 0; i < ul.children.length; i++) {
+        var li = this.document.createElement('li')
+        circle.appendChild(li)
+    }
 })
