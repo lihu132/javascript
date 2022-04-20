06f64e9fe37f0bdcb334b26f457ab90e467f2d74
leo-outside <morinbrit@gmail.com>
2022-04-20 15:29:08 +1000

    arrow_function

--- 317_swiper.js
+++ 317_swiper.js
@@ -8,7 +8,9 @@ window.addEventListener('load', function() {
     var img = this.document.querySelector('img')
 
     var imgWidth = img.offsetWidth
-    ul.style.width = img.offsetWidth * ul.children.length + 'px'
+    ul.style.width = imgWidth * ul.children.length + 'px'
+        // focus.style.width = imgWidth + 'px'
+        // focus.style.height = img.offsetHeighth + 'px'
 
     //1.鼠标经过focus 显示左右箭头
     focus.addEventListener('mouseenter', function() {
@@ -37,9 +39,39 @@ window.addEventListener('load', function() {
                 circle.children[i].className = ''
             }
             e.target.className = 'current'
+                //移动ul，移动图片
             animate(ul, -e.target.getAttribute('index') * imgWidth)
         }
         console.log(e.target.getAttribute('index'));
     })
 
+
+    //3.左右箭头
+    var clickCount = 0
+    var index = 0
+    arrow_l.addEventListener('click', function() {
+        // var leftPosition = ul.offsetLeft;
+        // animate(ul, leftPosition - imgWidth);
+        //animate因为有个interval时间，如果点击箭头太快，上一个动作为完成，下一个动作就开始，会导致错误位置
+        clickCount--
+        index++
+        var leftPosition = clickCount * imgWidth;
+        animate(ul, leftPosition)
+        for (var i = 0; i < ul.children.length; i++) {
+            circle.children[i].className = ''
+        }
+        circle.children[index].className = 'current'
+
+    })
+    arrow_r.addEventListener('click', function() {
+        clickCount++
+        index--
+        var leftPosition = clickCount * imgWidth;
+        animate(ul, leftPosition)
+        for (var i = 0; i < ul.children.length; i++) {
+            circle.children[i].className = ''
+        }
+        circle.children[index].className = 'current'
+
+    })
 })
