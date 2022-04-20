f597a947a287c4d01ef94334b51b2321e92f50eb
leo-outside <morinbrit@gmail.com>
2022-04-20 14:23:22 +1000

    circle_update

--- 317_swiper.js
+++ 317_swiper.js
@@ -3,9 +3,10 @@ window.addEventListener('load', function() {
     var arrow_l = this.document.querySelector('.arrow-l')
     var arrow_r = this.document.querySelector('.arrow-r')
     var focus = this.document.querySelector('.focus')
-    var ul = this.document.querySelector('ul')
+    var ul = focus.querySelector('ul')
     var circle = this.document.querySelector('.circle')
     var img = this.document.querySelector('img')
+
     ul.style.width = img.offsetWidth * ul.children.length + 'px'
 
     //1.鼠标经过focus 显示左右箭头
@@ -24,4 +25,17 @@ window.addEventListener('load', function() {
         var li = this.document.createElement('li')
         circle.appendChild(li)
     }
+
+    circle.children[0].className = 'current'
+
+    circle.addEventListener('click', function(e) {
+        if (e.target != circle) {
+            for (var i = 0; i < ul.children.length; i++) {
+                circle.children[i].className = ' '
+            }
+            e.target.className = 'current'
+        }
+        console.log(1);
+    })
+
 })
