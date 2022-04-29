88ce34a669bbef23ed86668d877ad16b57017ac5
leo-outside <morinbrit@gmail.com>
2022-04-29 10:29:10 +1000

    interval&transiotion

---
+++ 335_mobile_swiper.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+    var focus = this.document.querySelector('.focus');
+    var ul = focus.children[0];
+
+    var w = focus.offsetWidth;
+    var index = 0;
+    var timer = this.setInterval(function() {
+        index++
+        var translatex = -index * w;
+        ul.style.transition = 'all .3s'
+        ul.style.transform = 'translateX(' + translatex + 'px)';
+    }, 2000)
+})
