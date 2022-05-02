024fa469cf3aeff62bb3795195b524baed52b7f4
leo-outside <morinbrit@gmail.com>
2022-05-02 15:16:58 +1000

    no_gap_swip

--- 335_mobile_swiper.js
+++ 335_mobile_swiper.js
@@ -13,6 +13,22 @@ window.addEventListener('load', function() {
         //过渡完成之后，再去执行，transitioned
         ul.addEventListener('transitionend', function() {
             //无缝滚动
+            if (index == 3) {
+
+                index = 0;
+                //去掉过渡效果，让ul快速跳到目标位置
+                ul.style.transition = 'none';
+                //利用最新的索引号，滚动图片
+                var translatex = -index * w;
+                ul.style.transform = 'translateX(' + translatex + 'px)';
+            } else if (index < 0) {
+                index = 2;
+                //去掉过渡效果，让ul快速跳到目标位置
+                ul.style.transition = 'none';
+                //利用最新的索引号，滚动图片
+                var translatex = -index * w;
+                ul.style.transform = 'translateX(' + translatex + 'px)';
+            }
 
         })
     }, 2000)
