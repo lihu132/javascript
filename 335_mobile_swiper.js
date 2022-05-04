310c8e3b0a4295b69311b243259a16ea5011e9b3
lihu132 <lihu132@gmail.com>
2022-05-04 22:45:17 +1000

    touch_move

--- 335_mobile_swiper.js
+++ 335_mobile_swiper.js
@@ -11,48 +11,79 @@ window.addEventListener('load', function () {
         ul.style.transition = 'all .3s'
         ul.style.transform = 'translateX(' + translatex + 'px)';
 
-
-
-
-        //过渡完成之后，再去执行，transitioned
-        ul.addEventListener('transitionend', function () {
-            //无缝滚动
-            if (index >= 3) {
-
-                index = 0;
-                //去掉过渡效果，让ul快速跳到目标位置
-                ul.style.transition = 'none';
-                //利用最新的索引号，滚动图片
-                var translatex = -index * w;
-                ul.style.transform = 'translateX(' + translatex + 'px)';
-            } else if (index < 0) {
-                index = 2;
-                //去掉过渡效果，让ul快速跳到目标位置
-                ul.style.transition = 'none';
-                //利用最新的索引号，滚动图片
-                var translatex = -index * w;
-                ul.style.transform = 'translateX(' + translatex + 'px)';
-            }
-            //小圆点跟随变化
-            // for (var i = 0; i < circle.children.length; i++) {
-            //     circle.children[i].className = ''
-            // }
-            // circle.children[index].className = 'current'
-            console.log(index);
-            circle.querySelector('li.current').classList.remove('current')
-            circle.children[index].classList.add('current')
-        })
-
-
-
-
-
-
-
     }, 2000)
 
 
+    //过渡完成之后，再去执行，transitioned
+    ul.addEventListener('transitionend', function () {
+        //无缝滚动
+        if (index >= 3) {
 
+            index = 0;
+            //去掉过渡效果，让ul快速跳到目标位置
+            ul.style.transition = 'none';
+            //利用最新的索引号，滚动图片
+            var translatex = -index * w;
+            ul.style.transform = 'translateX(' + translatex + 'px)';
+        } else if (index < 0) {
+            index = 2;
+            //去掉过渡效果，让ul快速跳到目标位置
+            ul.style.transition = 'none';
+            //利用最新的索引号，滚动图片
+            var translatex = -index * w;
+            ul.style.transform = 'translateX(' + translatex + 'px)';
+        }
+        //小圆点跟随变化
+        // for (var i = 0; i < circle.children.length; i++) {
+        //     circle.children[i].className = ''
+        // }
+        // circle.children[index].className = 'current'
+        console.log(index);
+        circle.querySelector('li.current').classList.remove('current')
+        circle.children[index].classList.add('current')
+    })
+
+
+
+    //手指滑动轮播图
+    var startX = 0;
+    ul.addEventListener('touchstart', function (e) {
+        startX = e.targetTouches[0].pageX;
+        //手指触摸时停止定时器
+        clearInterval(timer)
+    })
+    //计算手指移动距离
+    var moveX = 0;
+    ul.addEventListener('touchmove', function (e) {
+        moveX = e.targetTouches[0].pageX - startX;
+        //移动盒子
+        var translatex = -index * w + moveX;
+
+        //手指移动不需要动画效果
+        ul.style.transition = 'none';
+        ul.style.transform = 'translateX(' + translatex + 'px)';
+
+    })
+
+    //手指离开后，根据移动距离判断是回弹还是播放上一张或者下一张
+    ul.addEventListener('touchend', function (e) {
+        //如果移动距离大于50px,则播放上一张或下一张
+        if (Math.abs(moveX) > 50) {
+            //如果右滑，播放上一张moveX是正值
+            if (moveX > 0) {
+                index--;
+            }
+            else {
+                index++;
+            }
+            //如果左滑，播放下一张moveX是负值
+            var translatex = -index * w;
+            ul.style.transition = 'all .3s'
+            ul.style.transform = 'translateX(' + translatex + 'px)';
+
+        }
+
+    })
 
 
 })
